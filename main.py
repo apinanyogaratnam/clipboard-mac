ec1b4c97c9c9ad7bda2fdd2f3c99f2ed8856ad8e
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-09 21:38:07 -0500

    feat: created clipboard

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import rumps
+import pyperclip
 
 
 class Clipboard:
     def __init__(self):
         self.app = rumps.App("Clipboard", "🍅")
-    def run(self):
+        self.clipboard_contents = [pyperclip.paste()]
+        self.timer = rumps.Timer(self.update_clipboard, 1)
+        self.timer.start()
+        self.app.menu = list(self.clipboard_contents)
+
         self.app.run()
+        self.app.run()
+
+    def update_clipboard(self, _):
+        new_clipboard_content = pyperclip.paste()
+        if new_clipboard_content not in self.clipboard_contents:
+            # if len(self.clipboard_contents) == 10:
+                # self.clipboard_contents.pop()
+            self.clipboard_contents.insert(0, new_clipboard_content)
+            self.app.menu = list(self.clipboard_contents)
 
 if __name__ == '__main__':
     app = Clipboard()
