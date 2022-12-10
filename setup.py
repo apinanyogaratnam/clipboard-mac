e25ffcb149f06f625d46acde73e8e848d459b511
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-09 22:35:01 -0500

    build: updated app name

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ OPTIONS = {
 
 setup(
     app=APP,
-    name='Pomodoro',
+    name='Clipboard',
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'], install_requires=['rumps', 'pyperclip']
