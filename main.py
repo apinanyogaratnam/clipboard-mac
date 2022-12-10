35dcc54b581858ac47b7e0dde8b1549fb0674d65
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-09 21:46:20 -0500

    fix: clipboard duplicates

--- main.py
+++ main.py
@@ -5,21 +5,18 @@ import pyperclip
 class Clipboard:
     def __init__(self):
         self.app = rumps.App("Clipboard", "🍅")
-        self.clipboard_contents = [pyperclip.paste()]
         self.timer = rumps.Timer(self.update_clipboard, 0.01)
         self.timer.start()
-        self.app.menu = list(self.clipboard_contents)
+        self.app.menu = []
 
-        self.app.run()
         self.app.run()
 
     def update_clipboard(self, _):
         new_clipboard_content = pyperclip.paste()
-        if new_clipboard_content not in self.clipboard_contents:
+        if new_clipboard_content not in self.app.menu:
             # if len(self.clipboard_contents) == 10:
                 # self.clipboard_contents.pop()
-            self.clipboard_contents.insert(0, new_clipboard_content)
-            self.app.menu = list(self.clipboard_contents)
+            self.app.menu.update([new_clipboard_content])
 
 if __name__ == '__main__':
     app = Clipboard()
