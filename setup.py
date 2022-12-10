22767558db25dca18dc1c8eb2245fd1ad4af1bf2
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-09 22:34:00 -0500

    build: created setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+APP = ['main.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'paperclip.ico',
+    'plist': {
+        'CFBundleShortVersionString': '0.2.0',
+        'LSUIElement': True,
+    },
+    'packages': ['rumps', 'pyperclip'],
+}
+
+setup(
+    app=APP,
+    name='Pomodoro',
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'], install_requires=['rumps', 'pyperclip']
+)
